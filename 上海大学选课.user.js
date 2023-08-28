c7c96bb40600a37c4e630cd2ed7a5aa6cf62c6e0
0x7a6a <2281979437@qq.com>
2023-08-28 12:27:27 +0800

    Add files via upload

--- 上海大学选课.user.js
+++ 上海大学选课.user.js
@@ -7,24 +7,29 @@
 // @match        https://yjsxk.shu.edu.cn/yjsxkapp/sys/xsxkapp/course.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=shu.edu.cn
 // @grant        none
+// @run-at context-menu
 // ==/UserScript==
 
-let timeout = 2
+let timeout = 1
 
 function xk()
 {
     var p=document.evaluate('/html/body/div/article[2]/div[3]/div/div/div[1]/div/div[2]/ul/li[3]/a',document).iterateNext();
     p.click();
-    var xpath = '/html/body/div/article[2]/div[3]/div/div/div[1]/div/table/tbody/tr[2]'
-    var cap = document.evaluate(xpath+'/td[8]/span', document).iterateNext();
-    var [cap_now, cap_total] = cap.textContent.split('/');
-    console.log(cap_now, cap_total);
-    if (cap_now < cap_total)
+    var ids = [2, 3, 4];
+    for (var k = 0; k<ids.length; k++)
     {
-        var xk_btn=document.evaluate(xpath+'/td[9]/a',document).iterateNext();
-        xk_btn.click();
-        var confirm_btn = document.evaluate('/html/body/div[3]/div[6]/div/button[1]',document).iterateNext();
-        confirm_btn.click();
+        var xpath = '/html/body/div/article[2]/div[3]/div/div/div[1]/div/table/tbody/tr['+ids[k]+']'
+        var cap = document.evaluate(xpath+'/td[8]/span', document).iterateNext();
+        var [cap_now, cap_total] = cap.textContent.split('/');
+        console.log(cap_now, cap_total);
+        if (cap_now < cap_total)
+        {
+            var xk_btn=document.evaluate(xpath+'/td[9]/a',document).iterateNext();
+            xk_btn.click();
+            var confirm_btn = document.evaluate('/html/body/div[3]/div[6]/div/button[1]',document).iterateNext();
+            confirm_btn.click();
+        }
     }
     setTimeout(() => {
         xk()
